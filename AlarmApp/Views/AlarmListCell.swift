6f315391cf8e3d9960b49043db3c12529c2a0516
bhumika-ios <bhumika.iosdeveloper.com>
2024-09-10 11:53:58 +0530

    add alaram screen with add and edit for create bottomsheet

---
+++ AlarmApp/Views/AlarmListCell.swift
@@ -0,0 +1,18 @@
+//
+//  AlarmListCell.swift
+//  AlarmApp
+//
+//  Created by Bhumika Patel on 07/09/24.
+//
+
+import SwiftUI
+
+struct AlarmListCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlarmListCell()
+}
---
+++ AlarmApp/Views/AlarmListView.swift
@@ -0,0 +1,18 @@
+//
+//  AlarmListView.swift
+//  AlarmApp
+//
+//  Created by Bhumika Patel on 07/09/24.
+//
+
+import SwiftUI
+
+struct AlarmListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlarmListView()
+}
