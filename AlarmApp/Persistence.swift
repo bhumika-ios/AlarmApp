6f8a43c97a94c479d2b15fa49e498b135c291f73
bhumika-ios <bhumika.iosdeveloper.com>
2024-09-10 11:56:22 +0530

    connect with data base and add and edit and delete

--- AlarmApp/Persistence.swift
+++ AlarmApp/Persistence.swift
@@ -4,53 +4,83 @@
 //
 //  Created by Bhumika Patel on 07/09/24.
 //
-
+import Foundation
 import CoreData
 
-struct PersistenceController {
-    static let shared = PersistenceController()
 
-    static var preview: PersistenceController = {
-        let result = PersistenceController(inMemory: true)
-        let viewContext = result.container.viewContext
-        for _ in 0..<10 {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-        }
-        do {
-            try viewContext.save()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nsError = error as NSError
-            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-        }
-        return result
-    }()
-
-    let container: NSPersistentContainer
-
-    init(inMemory: Bool = false) {
-        container = NSPersistentContainer(name: "AlarmApp")
-        if inMemory {
-            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
-        }
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
+final class CoreDataManager {
+    
+    static let shared = CoreDataManager()
+    
+    let persistentContainer: NSPersistentContainer
+    
+    init() {
+        persistentContainer = NSPersistentContainer(name: "AlarmApp")
+        persistentContainer.loadPersistentStores { (description, error) in
+            if let error = error {
+                fatalError("Unable to load persistent stores: \(error)")
             }
-        })
-        container.viewContext.automaticallyMergesChangesFromParent = true
+        }
+    }
+    
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+
+    
+    func fetchAlarms() -> [AlarmList] {
+        let fetchRequest: NSFetchRequest<AlarmList> = AlarmList.fetchRequest()
+        do {
+            return try persistentContainer.viewContext.fetch(fetchRequest)
+        } catch {
+            print("Error fetching alarms: \(error)")
+            return []
+        }
+    }
+    
+    func addAlarm(time: String, label: String, isActive: Bool, date: Date?, day: String?) {
+        let newAlarm = AlarmList(context: persistentContainer.viewContext)
+        newAlarm.id = UUID()
+        newAlarm.day = day
+        newAlarm.date = date
+        newAlarm.alarmDiscription = label
+        newAlarm.isActiveAlarm = isActive
+        
+        // Save hours and minutes
+        if let timeComponents = time.split(separator: ":").map(String.init) as [String]? {
+            newAlarm.hours = Int16(timeComponents[0]) ?? 0
+            newAlarm.minutes = Int16(timeComponents[1].prefix(2)) ?? 0
+        }
+        
+        saveContext()
+    }
+
+    func updateAlarm(alarm: AlarmList, time: String, label: String, isActive: Bool, date: Date?, day: String?) {
+        alarm.day = day
+        alarm.date = date
+        alarm.alarmDiscription = label
+        alarm.isActiveAlarm = isActive
+        
+        // Update hours and minutes
+        if let timeComponents = time.split(separator: ":").map(String.init) as [String]? {
+            alarm.hours = Int16(timeComponents[0]) ?? 0
+            alarm.minutes = Int16(timeComponents[1].prefix(2)) ?? 0
+        }
+        
+        saveContext()
+    }
+
+    
+    func deleteAlarm(alarm: AlarmList) {
+        persistentContainer.viewContext.delete(alarm)
+        saveContext()
     }
 }
--- AlarmApp/Views/AlarmListCell.swift
+++ AlarmApp/Views/AlarmListCell.swift
@@ -4,15 +4,60 @@
 //
 //  Created by Bhumika Patel on 07/09/24.
 //
-
 import SwiftUI
 
 struct AlarmListCell: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+    @Binding var isOn: Bool
+  
+    let alarm: AlarmList
 
-#Preview {
-    AlarmListCell()
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text("\(alarm.hours):\(String(format: "%02d", alarm.minutes))")
+                    .font(.largeTitle)
+                    .fontWeight(.light)
+                
+                Text(alarm.alarmDiscription ?? "No Label")
+                    .fontWeight(.thin)
+              
+                if let date = alarm.date {
+                    Text("Date: \(formattedDate(date))")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                } else {
+                    Text(alarm.day ?? "No Days")
+                        .font(.caption)
+                        .foregroundColor(.gray)
+                }
+            }
+            
+            Spacer()
+            
+            HStack {
+             
+                    Toggle("", isOn: $isOn)
+                        .labelsHidden()
+                        .tint(.red)
+                        .onChange(of: isOn) { newValue in
+                            if newValue {
+                                print("Toggle is ON")
+                            } else {
+                                print("Toggle is OFF")
+                            }
+                        }
+                }
+            
+        }
+        .padding(.leading, 2)
+        .padding(.trailing, 2)
+        .frame(maxWidth: .infinity)
+    }
+
+    private func formattedDate(_ date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        return formatter.string(from: date)
+    }
 }
